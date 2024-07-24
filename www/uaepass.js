6614da44696dfd5fb9bf021d29e42c357bc874e8
GEMS Education <59133384+GEMSEDU@users.noreply.github.com>
2024-07-24 15:50:33 +0400

    Update uaepass.js

--- www/uaepass.js
+++ www/uaepass.js
@@ -1,29 +1,47 @@
 var exec = require('cordova/exec');
 
-exports.init = function (success, error,environment,clientID,clientSecret,redirectUrl) {
-    exec(success, error, 'uaepass', 'initPlugin', [environment,clientID,clientSecret,redirectUrl]);
+var UAEPass = {
+  init: function (environment, clientID, clientSecret, redirectUrl) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, 'uaepass', 'initPlugin', [environment, clientID, clientSecret, redirectUrl]);
+    });
+  },
+
+  getWritePermission: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, 'uaepass', 'getWritePermission', []);
+    });
+  },
+
+  getCode: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, 'uaepass', 'getCode', []);
+    });
+  },
+
+  getAccessToken: function (code) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, 'uaepass', 'getAccessToken', [code]);
+    });
+  },
+
+  getProfile: function (accessToken) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, 'uaepass', 'getProfile', [accessToken]);
+    });
+  },
+
+  signDocument: function (documentURL) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, 'uaepass', 'signDocument', [documentURL]);
+    });
+  },
+
+  clearData: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, 'uaepass', 'clearData', []);
+    });
+  }
 };
 
-exports.getWritePermission = function (success, error) {
-    exec(success, error, 'uaepass', 'getWritePermission', []);
-};
-
-exports.getCode = function (success, error) {
-    exec(success, error, 'uaepass', 'getCode', []);
-};
-
-exports.getAccessToken = function (success, error,code) {
-    exec(success, error, 'uaepass', 'getAccessToken', [code]);
-};
-
-exports.getProfile = function (success, error,accessToken) {
-    exec(success, error, 'uaepass', 'getProfile', [accessToken]);
-};
-
-exports.signDocument = function (success, error,documentURL) {
-    exec(success, error, 'uaepass', 'signDocument', [documentURL]);
-};
-
-exports.clearData = function (success, error) {
-    exec(success, error, 'uaepass', 'clearData', []);
-};
+module.exports = UAEPass;
